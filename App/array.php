f5804124c616e133a19811860692470f517a7745
Martin Pucan <martin.pucan@pilulka.cz>
2020-06-01 16:34:19 +0200

    train with function - need remove structure

--- App/array.php
+++ App/array.php
@@ -1,5 +1,23 @@
 <?php
 
-// trim and strlen
+$array = [
+    'else',
+    'non',
+    'good',
+    'great',
+    'great',
+    'non',
+];
 
-$_POST['postcode'];
+$result = array_unique($array);
+var_dump($result);
+
+$input = [100=>"red", 'yellow', 2=>"green", 3=>"blue", 4=>"yellow"];
+var_dump(array_slice($input, 0, 1)[0]);
+var_dump($input);
+var_dump(array_merge($input, [124, 125, 125,125,125, 12,5125]));
+
+$a = array_fill(5, 6, 'banana');
+$b = array_fill(-2, 4, 'pear');
+print_r($a);
+print_r($b);
